c84744c47f53321d7865a21b26fe468bcec779f4
MP <markpaololibunao@gmail.com>
2022-10-21 04:06:02 +0800

    Switches to my middleware implementation since it doesn't fail eslint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,14 @@
-//import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
-
-//export async function middleware(
-//req: NextRequest,
-//_event: NextFetchEvent
-//): Promise<Response | undefined> {
-//if (req.nextUrl.pathname === '/') {
-//console.log('req.nextUrl.pathname', req.nextUrl) // eslint-disable-line no-console
-//return NextResponse.next()
-//}
-
-//const slug = req.nextUrl.pathname.split('/').pop()
-//console.log('slug', slug) // eslint-disable-line no-console
-
-//const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
-//if (slugFetch.status === 404) {
-//return NextResponse.redirect(req.nextUrl.origin)
-//}
-//const data = await slugFetch.json()
-
-//if (data?.url) {
-//return NextResponse.redirect(data.url)
-//}
-
-//return NextResponse.next()
-//}
-
-//export const config = {
-//matcher: '/:slug',
-//}
-
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(req: NextRequest, _ev: NextFetchEvent) {
-	console.log('req.nextUrl.pathname', req.nextUrl) // eslint-disable-line no-console
+export async function middleware(
+	req: NextRequest,
+	_event: NextFetchEvent
+): Promise<Response | undefined> {
+	console.log('req.nextUrl.pathname', req.nextUrl.pathname) // eslint-disable-line no-console
 	if (req.nextUrl.pathname === '/') {
 		return NextResponse.next()
 	}
+
 	const slug = req.nextUrl.pathname.split('/').pop()
 	console.log('slug', slug) // eslint-disable-line no-console
 
@@ -48,8 +21,35 @@ export async function middleware(req: NextRequest, _ev: NextFetchEvent) {
 	if (data?.url) {
 		return NextResponse.redirect(data.url)
 	}
+
+	return NextResponse.next()
 }
 
 export const config = {
 	matcher: ['/:slug'],
 }
+
+//import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+
+//export async function middleware(req: NextRequest, _ev: NextFetchEvent) {
+//console.log('req.nextUrl.pathname', req.nextUrl) // eslint-disable-line no-console
+//if (req.nextUrl.pathname === '/') {
+//return NextResponse.next()
+//}
+//const slug = req.nextUrl.pathname.split('/').pop()
+//console.log('slug', slug) // eslint-disable-line no-console
+
+//const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
+//if (slugFetch.status === 404) {
+//return NextResponse.redirect(req.nextUrl.origin)
+//}
+//const data = await slugFetch.json()
+
+//if (data?.url) {
+//return NextResponse.redirect(data.url)
+//}
+//}
+
+//export const config = {
+//matcher: ['/:slug'],
+//}
