f1bb1005d8b1918877248c938d8195267b128bb3
MP <markpaololibunao@gmail.com>
2022-10-21 09:29:54 +0800

    Seems this is really broken anyway so I'll just put my code then come back to rewrite it later

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,6 @@ export async function middleware(
 	req: NextRequest,
 	_event: NextFetchEvent
 ): Promise<Response | undefined> {
-	console.log('req.nextUrl.pathname', req.nextUrl.pathname) // eslint-disable-line no-console
 	if (req.nextUrl.pathname === '/') {
 		return NextResponse.next()
 	}
@@ -28,28 +27,3 @@ export async function middleware(
 export const config = {
 	matcher: ['/', '/:slug*'],
 }
-
-//import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
-
-//export async function middleware(req: NextRequest, _ev: NextFetchEvent) {
-//console.log('req.nextUrl.pathname', req.nextUrl) // eslint-disable-line no-console
-//if (req.nextUrl.pathname === '/') {
-//return NextResponse.next()
-//}
-//const slug = req.nextUrl.pathname.split('/').pop()
-//console.log('slug', slug) // eslint-disable-line no-console
-
-//const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
-//if (slugFetch.status === 404) {
-//return NextResponse.redirect(req.nextUrl.origin)
-//}
-//const data = await slugFetch.json()
-
-//if (data?.url) {
-//return NextResponse.redirect(data.url)
-//}
-//}
-
-//export const config = {
-//matcher: ['/:slug'],
-//}
--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -1,12 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '@/server/infra/db'
 
-export async function getUrl0(req: NextApiRequest, res: NextApiResponse) {
+export default async function getUrl(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
 	const slug = req.query.slug
 
-	//res.setHeader('Content-Type', 'application/json')
-	//res.setHeader('Access-Control-Allow-Origin', '*')
-	//res.setHeader('Cache-Control', 's-maxage=604800, stale-while-revalidate') // 1 week
+	res.setHeader('Content-Type', 'application/json')
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Cache-Control', 's-maxage=604800, stale-while-revalidate') // 1 week
 
 	/*
 	 *https://vercel.com/docs/concepts/edge-network/caching#cacheable-responses
@@ -27,41 +30,4 @@ export async function getUrl0(req: NextApiRequest, res: NextApiResponse) {
 	}
 
 	res.json(data)
-}
-
-export default async function getUrl(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
-	const slug = req.query['slug']
-
-	if (!slug || typeof slug !== 'string') {
-		res.statusCode = 404
-
-		res.send(JSON.stringify({ message: 'pls use with a slug' }))
-
-		return
-	}
-
-	const data = await db.shortLink.findFirst({
-		where: {
-			slug: {
-				equals: slug,
-			},
-		},
-	})
-
-	if (!data) {
-		res.statusCode = 404
-
-		res.send(JSON.stringify({ message: 'slug not found' }))
-
-		return
-	}
-
-	//res.setHeader('Content-Type', 'application/json')
-	//res.setHeader('Access-Control-Allow-Origin', '*')
-	//res.setHeader('Cache-Control', 's-maxage=1000000000, stale-while-revalidate')
-
-	return res.json(data)
 }
